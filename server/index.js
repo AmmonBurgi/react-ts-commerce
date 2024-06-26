e9840336895ceffe96b61b99043c6814ca86eafa
Ammon Burgi <ammon@indicio.tech>
2024-06-26 17:02:54 -0600

    feat: setting up deployment with vercel

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = 4040
 
 // -------Middleware-------
 
 app.use(express.json())
+// app.use(express.static(__dirname + '/../public'))
 app.use(express.static(__dirname + '/../build'))
 
 // -------Middleware-------
@@ -15,7 +15,8 @@ app.get('/api/ping', (req, res) => res.send("Hello, I'm the server!"))
 
 // Serve build of react application
 app.get('*', (req, res) => {
+  console.log("Serving Build!")
   res.sendFile(path.join(__dirname, '../build/index.html'))
 });
 
-app.listen(4040, () => console.log(`Server listening on port ${port}`))
+module.exports = app
--- src/Components/ShopPage.tsx
+++ src/Components/ShopPage.tsx
@@ -3,8 +3,7 @@ import { Outlet } from "react-router-dom"
 export default function ShopPage() {
   return (
     <div>
-      <p>Shope Page</p>
-
+      <p>Shop Page</p>
       <Outlet />
     </div>
   )
